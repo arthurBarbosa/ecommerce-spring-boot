b74ccaa7ce9b2b21655cc176953af8e085a7c0f4
Arthur Barbosa <arthurbruno03@gmail.com>
2021-03-02 01:56:37 -0300

    Fix: update version 2.4.0

--- src/test/java/com/abcode/ecommerce/SpringBootEcommerceApplicationTests.java
+++ src/test/java/com/abcode/ecommerce/SpringBootEcommerceApplicationTests.java
@@ -1,6 +1,6 @@
 package com.abcode.ecommerce;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringBootEcommerceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
