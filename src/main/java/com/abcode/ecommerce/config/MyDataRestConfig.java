27d0bd219ae5b4967f32dbefc14976a010bfe93e
Arthur Bruno Moreira Barbosa <arthur.barbosa@ITBL000531D.itbam.org.br>
2021-04-07 16:05:22 -0400

    Refactoring - config cors

--- src/main/java/com/abcode/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/abcode/ecommerce/config/MyDataRestConfig.java
@@ -2,10 +2,12 @@ package com.abcode.ecommerce.config;
 
 import com.abcode.ecommerce.entity.Product;
 import com.abcode.ecommerce.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import javax.persistence.EntityManager;
 import javax.persistence.metamodel.EntityType;
@@ -16,6 +18,9 @@ import java.util.Set;
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
+    @Value("${allowed.origins}")
+    private String[] theAllowedOrigins;
+
     private EntityManager entityManager;
 
     public MyDataRestConfig(EntityManager entityManager){
@@ -23,8 +28,8 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     }
 
     @Override
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-        HttpMethod[] theUnSupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnSupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
 
         // disable HTTP methods for Product: PUT, POST and DELETE
         config.getExposureConfiguration()
@@ -40,6 +45,9 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
         // call an internal helper method
         exposeIds(config);
+
+        // configure cors mapping
+        cors.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigins);
 
     }
 
--- src/main/java/com/abcode/ecommerce/controller/CheckoutController.java
+++ src/main/java/com/abcode/ecommerce/controller/CheckoutController.java
@@ -3,21 +3,23 @@ package com.abcode.ecommerce.controller;
 import com.abcode.ecommerce.dto.Purchase;
 import com.abcode.ecommerce.dto.PurchaseResponse;
 import com.abcode.ecommerce.service.CheckoutService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
 
     private CheckoutService checkoutService;
 
-    public CheckoutController(CheckoutService checkoutService){
+    public CheckoutController(CheckoutService checkoutService) {
         this.checkoutService = checkoutService;
     }
 
     @PostMapping("/purchase")
-    public PurchaseResponse placeOrder(@RequestBody Purchase purchase){
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
 
         PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
 
